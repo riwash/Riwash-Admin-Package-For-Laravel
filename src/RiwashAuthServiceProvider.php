2c24ba008a5ce487477d03210e65991cac5253f3
unknown <technicalriwash@gmail.com>
2024-02-12 16:33:48 +0545

    setup login

--- src/RiwashAuthServiceProvider.php
+++ src/RiwashAuthServiceProvider.php
@@ -15,6 +15,7 @@ class RiwashAuthServiceProvider extends ServiceProvider
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/Views', 'riwashauth');
         $this->publishes([
             __DIR__ . '/config/riwashadmin.php' => config_path('riwashadmin.php'),
         ], 'config');
